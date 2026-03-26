e4dbf9ad76ebfdbc3f2a25abdfee5ce48f59d0df
sofy2831 <s.dumas974@orange.fr>
2026-03-26 12:18:20 +0100

    Create firebase-config.js

---
+++ admin/firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAvC4Hae3huW_bpriic8RrGhHkfwD13dVs",
+  authDomain: "axe-dossier-prod.firebaseapp.com",
+  projectId: "axe-dossier-prod",
+  storageBucket: "axe-dossier-prod.firebasestorage.app",
+  messagingSenderId: "930002685598",
+  appId: "1:930002685598:web:ccdc67eee45676aa28358f"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
